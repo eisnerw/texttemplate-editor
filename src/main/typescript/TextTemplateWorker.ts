c155cdff9ec88e8a1615ce2858fbe4eba544370f
eisnerw <eisnerw@hotmail.com>
2020-09-25 16:06:10 -0400

    Web worker implementation clean

--- src/main/typescript/TextTemplateWorker.ts
+++ src/main/typescript/TextTemplateWorker.ts
@@ -1,41 +1,6 @@
 const ctx: Worker = self as any;
 import textTemplateInterpreter = require("../../main-generated/javascript/TextTemplateInterpreter.js");
-class SieveOfEratosthenes {
-  
-    calculate(limit: number) {
 
-      const sieve = [];
-      const primes: number[] = [];
-      let k;
-      let l;
-
-      sieve[1] = false;
-      for (k = 2; k <= limit; k += 1) {
-        sieve[k] = true;
-      }
-
-      for (k = 2; k * k <= limit; k += 1) {
-        if (sieve[k] !== true) {
-          continue;
-        }
-        for (l = k * k; l <= limit; l += k) {
-          sieve[l] = false;
-        }
-      }
-
-      sieve.forEach(function (value, key) {
-        if (value) {
-          this.push(key);
-        }
-      }, primes);
-
-      return primes;
-
-    }
-
-}
-
-const sieve = new SieveOfEratosthenes();
 let urlParms = {};
 let parmId = 0;
 let input = null;
@@ -80,12 +45,6 @@ ctx.addEventListener("message", (event) => {
 		case 'url':
 			urlParms[payload.id].success(payload.data);
 			delete urlParms[payload.id];
-			break;
-
-		case 'primes':
-			const limit = payload.limit;
-			const primes = sieve.calculate(limit);
-			ctx.postMessage({type: 'result', result: primes });
 			break;
 	}
 });
